1bd613b66559ec65ec20954a4550d63ff9fc3c5f
Carlos Hoyos <choyos@cifpfbmoll.eu>
2021-11-29 16:06:49 +0100

    Ejercicio 3

---
+++ ejercicio_3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookie incremento</title>
+</head>
+<body>
+    El numero entero positivo más grande que se puede representar en PHP es:
+    <?php
+    echo(PHP_INT_MAX);
+    echo "<br>";
+    echo( 'Lo podemos obtener con la constante definida PHP_INT_MAX y su valor varia para sistemas de 32bits');
+    ?>
+    
+</body>
+</html>
